4a5514e3f71904bd9b24dc2e182471e7bfb5cfaf
Calvin Boyce <calvinrboyce@gmail.com>
2017-12-08 08:10:41 -0700

    first commit

---
+++ Ch1L1_Poem.java
@@ -0,0 +1,13 @@
+//Ch1L1 Poem
+//Calvin Boyce
+//8/30/17
+//Coah Tua'one
+//Comp Sci 2 per 1
+public class Ch1L1_Poem{
+    public static void main(String args[]){
+        System.out.println("Roses are red");
+        System.out.println("Violets are blue");
+        System.out.println("Sugar is sweet");
+        System.out.println("And so are you!");
+    }
+}
