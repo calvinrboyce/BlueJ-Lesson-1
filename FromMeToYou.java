45d10f4dd3c20bb5d65c228a676929bee8e2fdae
Samuel Sanft <sdsanft@gmail.com>
2017-12-08 08:58:30 -0700

    fixed date

--- FromMeToYou.java
+++ FromMeToYou.java
@@ -4,7 +4,7 @@ public class FromMeToYou{
     public static void main(String args[]){
         System.out.println("From: Bill Smith");
         System.out.println("Address: Dell Computer, Bldg13");
-        System.out.println("Date: April 12, 2005");
+        System.out.println("Date: December 8, 2017");
         System.out.println("To: Jack Jones");
         System.out.println("Message: Help! I'm trapped inside a computer!");
     }
