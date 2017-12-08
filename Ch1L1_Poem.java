04a54c1024cbbb45b7bd0ace3440fa65f79ef2d4
Calvin Boyce <calvinrboyce@gmail.com>
2017-12-08 08:20:05 -0700

    new haiku

--- Ch1L1_Poem.java
+++ Ch1L1_Poem.java
@@ -5,9 +5,8 @@
 //Comp Sci 2 per 1
 public class Ch1L1_Poem{
     public static void main(String args[]){
-        System.out.println("Roses are red");
-        System.out.println("Violets are blue");
-        System.out.println("Sugar is sweet");
-        System.out.println("And so are you!");
+        System.out.println("This is a haiku");
+        System.out.println("It has lots of syllables");
+        System.out.println("That was my haiku");
     }
 }
